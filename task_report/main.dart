9a3211f64d71598da14c3a1a192576c7cd28deae
Eduardo <martinsbatista.eduardo@gmail.com>
2026-05-26 19:53:15 -0300

    construção das classes Tarefa e ItemTrabalho.

--- task_report/main.dart
+++ task_report/main.dart
@@ -1,19 +1,43 @@
-class Tarefa {
-  int id;
-  String titulo;
+class Tarefa extends ItemTrabalho{
+ 
   String responsavel;
   String status;
   String prioridade;
   double valor;
   int horas;
 
-  Tarefa({
-    required this.id,
-    required this.titulo,
+  Tarefa ({
+   required super.id,
+   required super.titulo,
     required this.responsavel,
     required this.status,
     required this.prioridade,
     required this.valor,
     required this.horas,
   });
+
+  Tarefa.fromMap(Map<String, dynamic> map)
+      : responsavel = map['responsavel'] ?? 'Sem responsável',
+        status = map['status'],
+        prioridade = map['prioridade'],
+        valor = map['valor'],
+        horas = map['horas'],
+        super.fromMap(map);
+}
+
+class ItemTrabalho {
+
+  String id;
+  String titulo;
+
+  ItemTrabalho ({
+
+    required this.id,
+    required this.titulo, 
+});
+
+ItemTrabalho.fromMap(Map<String, dynamic> map)
+: id = map['id'],
+titulo = map['titulo'];
+
 }
